21e6dd174f33966bc2c97ede2bcefe5dd92cfea8
Gng <guidonguido@gmail.com>
2020-01-25 17:23:54 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+import person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Guido', age: '22'},
+      {name: 'Petre', age: '21'}
+    ]
+  }
+
+  switchNameHandler = () => {
+
+    this.setState({
+      persons: [
+      {name: 'Guidonguido', age: '22'},
+      {name: 'PetreRic', age: '21'}
+      ]
+    });
+
+  }
+
+  render() {            //si usa className per accedere alle classi CSS perchè class è già usato per la classe JS
+    return (
+      <div className="App">
+        <h1> Ciao, sono Guido e sviluppo un'App React</h1>
+        <p> Ricorda che questo non è testo HTLM ma JSX, 
+            per questo si usa className per riferirsi alle classi CSS </p>
+        <button onClick={this.switchNameHandler}>Visualizza i Nickname</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} > Mi piacciono gli unicorni </Person>
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age} />
+        <Person/>
+      </div>
+    );
+
+    /*  In realtà è codice JSX, il compilatore lo compila come
+        return React.createElement('div', {className: App}, React.createElement('h1', null,'Ciao...'));
+    */
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Note from './Note'
+import './Person.css'
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <p> Sono il componente Persona, mi chiamo {props.name} e ho {props.age} anni. 
+            Ecco un numero random: {Math.random()*10} </p>
+            <Note content={props.children}/>
+        </div>
+        )
+};
+
+export default person;
